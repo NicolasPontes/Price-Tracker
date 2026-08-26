b2e92ac8c24f6b742f2962edc326c7139d2cfa73
NicolasPontes <kilowen3@gmail.com>
2026-08-26 15:41:39 -0300

    Criando as tabelas necessarias no models.py

---
+++ app/database/init_db.py
@@ -0,0 +1,9 @@
+from app.database.connection import engine
+from app.database.models import Base
+
+def criar_tabelas():
+    Base.metadata.create_all(bind=engine)
+    print("✅ Tabelas criadas com sucesso!")
+
+if __name__ == "__main__":
+    criar_tabelas() 
---
+++ app/database/models.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+from decimal import Decimal
+
+from sqlalchemy import Boolean, DateTime, ForeignKey, Numeric, String, Text
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+
+class Base(DeclarativeBase):
+    pass
+
+class Produto(Base):
+    __tablename__ = "produtos"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    nome: Mapped[str] = mapped_column(String(255), nullable=False)
+    url: Mapped[str] = mapped_column(Text, nullable=False)
+    preco_atual: Mapped[Decimal] = mapped_column(Numeric(10, 2), nullable=True)
+    preco_alvo: Mapped[Decimal] = mapped_column(Numeric(10, 2), nullable=False)
+
+    ativo: Mapped[bool] = mapped_column(Boolean, default=True, nullable=False)
+
+    criado_em: Mapped[datetime] = mapped_column(DateTime,default=datetime.utcnow,nullable=False)
+    atualizado_em: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow,onupdate=datetime.utcnow,nullable=False)
+    historico_precos: Mapped[list["HistoricoPreco"]] = relationship(back_populates="produto", cascade="all, delete-orphan")
+
+class HistoricoPreco(Base):
+    __tablename__ = "historico_precos"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    produto_id: Mapped[int] = mapped_column(ForeignKey("produtos.id"), nullable=False)
+
+    preco: Mapped[Decimal] = mapped_column(Numeric(10, 2), nullable=False)
+    coletado_em: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow, nullable=False)
+    produto: Mapped["Produto"] = relationship(back_populates="historico_precos")
